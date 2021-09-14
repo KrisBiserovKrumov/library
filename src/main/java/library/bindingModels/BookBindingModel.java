f133ece36c35a752bd5eca9a5dfc292226b45909
KrisBiserovKrumov <79319583+KrisBiserovKrumov@users.noreply.github.com>
2021-09-14 15:35:44 +0300

    Initial commit

---
+++ src/main/java/library/bindingModels/BookBindingModel.java
@@ -0,0 +1,34 @@
+package library.bindingModels;
+
+public class BookBindingModel {
+   private String title;
+   private String author;
+   private Integer price;
+
+    public BookBindingModel() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/library/entities/Book.java
@@ -0,0 +1,48 @@
+package library.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+  private Integer id;
+  private String title;
+  private String author;
+  private Integer price;
+
+    public Book() {
+    }
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
